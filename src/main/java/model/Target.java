73779ef349eb303b29fd8deed2c0aad2d868c3fd
njk nkl <baev-lev1@mail.ru>
2022-07-08 03:59:13 +0700

    added models with relationships

--- src/main/java/model/Target.java
+++ src/main/java/model/Target.java
@@ -1,7 +1,12 @@
 package model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Date;
 import java.util.UUID;
@@ -12,16 +17,18 @@ import java.util.UUID;
 @Builder
 @EqualsAndHashCode(of = {"guid"})
 @Entity
-@ToString(exclude = {"userGuid","contentGuid"})
+@ToString(exclude = {"content"})
 @Table(name = "target")
 public class Target {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID guid;
-    @Column(name = "user_guid")
-    private UUID userGuid;
-    @Column(name = "content_guid")
-    private UUID contentGuid;
+    @JoinColumn(name = "user_guid")
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+    @JoinColumn(name = "content_guid")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Content content;
     @Column
     private Page page;
     @Column
--- src/main/java/model/Viewed.java
+++ src/main/java/model/Viewed.java
@@ -1,6 +1,14 @@
 package model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,8 +26,10 @@ public class Viewed {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID guid;
-    @Column(name = "user_guid")
-    private UUID userGuid;
-    @Column(name = "content_guid")
-    private UUID contentGuid;
+    @JoinColumn(name = "user_guid")
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+    @JoinColumn(name = "content_guid")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Content content;
 }
